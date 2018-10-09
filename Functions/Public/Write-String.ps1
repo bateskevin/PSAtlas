a80a3c81b3c5d3804843f87691d59b71c754b930
Ichigo49 <Ichigo49@users.noreply.github.com>
2018-10-09 22:59:52 +0200

    Added some help to Write-String

--- Functions/Public/Write-String.ps1
+++ Functions/Public/Write-String.ps1
@@ -1,4 +1,29 @@
 Function Write-String {
+	<#
+		.SYNOPSIS
+			Write the content of a file to the screen
+			
+		.DESCRIPTION
+			Write in color the content of a file to the screen
+			
+		.PARAMETER Path
+			The path to the file
+			
+		.PARAMETER Type
+			Type of data to process, will determine the different colors to write in.
+			
+		.EXAMPLE
+			Write-String -Path C:\temp\myData.txt -Type Info
+			Write the content of myData.txt to the screen in green
+			
+		.EXAMPLE
+			Write-String -Path C:\temp\StuffToDo.txt -Type Task
+			Write the content of StuffToDo.txt to the screen in cyan
+			
+		.NOTES
+		   AUTHOR: Kevin BATES
+		   LASTEDIT: 09/10/2018(Ichigo49)
+	#>
     param(
         $Path,
         $Type
