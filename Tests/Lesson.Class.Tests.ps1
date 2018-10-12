20e1c5d6085718d1015f40aef2ba8a33d30f52a1
Kevin Bates <31886373+bateskevin@users.noreply.github.com>
2018-10-12 15:34:25 +0200

    Update Lesson.Class.Tests.ps1

--- Tests/Lesson.Class.Tests.ps1
+++ Tests/Lesson.Class.Tests.ps1
@@ -8,42 +8,37 @@ set-location -Path $RootFolder.FullName
 
 Import-module "./PWSHSchool" -force
 
-  $Path = "$PSScriptRoot/../Classes/Private"
-  $PrivateClasses = gci "$Path" -Filter "*.Class.ps1" | Select -Expand FullName | sort -Descending
-    foreach($c in $PrivateClasses){
-        . $c
-    }
+InModuleScope PWSHSchool {
+  Describe "Testing Class PWSHSchool Lesson" {
+      Context "Base functionality" {
+          it "[PWSHSchool][Class][Lesson] Creating an instance" {
+              {[Lesson]::new("./Lessons/Variable_Datatypes/Lesson.json")} | should not throw
+          }
+      }
 
+      Context "Properties" {
 
-Describe "Testing Class PWSHSchool Lesson" {
-    Context "Base functionality" {
-        it "[PWSHSchool][Class][Lesson] Creating an instance" {
-            {[Lesson]::new("./Lessons/Variable_Datatypes/Lesson.json")} | should not throw
-        }
-    }
+          $Lesson = [Lesson]::new("./Lessons/Variable_Datatypes/Lesson.json")
 
-    Context "Properties" {
+          it "[PWSHSchool][Class][Lesson] The Name Property should not be empty" {
+              $Lesson.Name | should not BeNullOrEmpty
+          }
 
-        $Lesson = [Lesson]::new("./Lessons/Variable_Datatypes/Lesson.json")
+          it "[PWSHSchool][Class][Lesson] The Name Property should be of type 'String'" {
+              $Lesson.Name | should BeOfType String
+          }
 
-        it "[PWSHSchool][Class][Lesson] The Name Property should not be empty" {
-            $Lesson.Name | should not BeNullOrEmpty
-        }
-        
-        it "[PWSHSchool][Class][Lesson] The Name Property should be of type 'String'" {
-            $Lesson.Name | should BeOfType String
-        }
+          it "[PWSHSchool][Class][Lesson] The Level Property should not be empty" {
+              $Lesson.Level | should not BeNullOrEmpty
+          }
 
-        it "[PWSHSchool][Class][Lesson] The Level Property should not be empty" {
-            $Lesson.Level | should not BeNullOrEmpty
-        }
+          it "[PWSHSchool][Class][Lesson] The Artifacts Property should not be empty" {
+              $Lesson.Artifacts | should not BeNullOrEmpty
+          }
 
-        it "[PWSHSchool][Class][Lesson] The Artifacts Property should not be empty" {
-            $Lesson.Artifacts | should not BeNullOrEmpty
-        }
-        
-        it "[PWSHSchool][Class][Lesson] The Prerequisites Property should not be empty" {
-            $Lesson.Prerequisites | should not BeNullOrEmpty
-        }
-    }
+          it "[PWSHSchool][Class][Lesson] The Prerequisites Property should not be empty" {
+              $Lesson.Prerequisites | should not BeNullOrEmpty
+          }
+      }
+  }
 }
